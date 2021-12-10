81819749322dc9c6f0109be1c5d0d421b96377f7
kimutaiamos <amos.kimutai@student.moringashool.com>
2021-12-10 21:27:47 +0300

    added profile table

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class Profile(models.Model):
+    #   profile_picture = CloudinaryField('image')
+    bio = models.TextField(max_length=600, default="My Bio", blank=True)
+    contact = models.CharField(max_length=60, blank=True)
+    timestamp = models.DateTimeField(default=timezone.now())
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", primary_key=True)
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+            if created:
+                Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+            instance.profile.save()
+
+    def __str__(self):
+         return self.user.username
+
+    def save_profile(self):
+            self.save()
+
+    def delete_profile(self):
+            self.delete()  
