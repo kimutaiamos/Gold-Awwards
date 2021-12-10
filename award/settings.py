a31652968eb943da0f69d5aa827e6a92a2855823
kimutaiamos <amos.kimutai@student.moringashool.com>
2021-12-10 17:05:27 +0300

    added index file to display the homepage

--- award/settings.py
+++ award/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'awards.apps.AwardsConfig',
     'bootstrap4',
+    'tinymce',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
