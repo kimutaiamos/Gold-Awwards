2a0a0dce249d0c5baafb278b4ae9f6337bcc5a6e
kimutaiamos <amos.kimutai@student.moringashool.com>
2021-12-10 21:40:37 +0300

    added table to display projects

--- awards/models.py
+++ awards/models.py
@@ -9,7 +9,7 @@ class Profile(models.Model):
     #   profile_picture = CloudinaryField('image')
     bio = models.TextField(max_length=600, default="My Bio", blank=True)
     contact = models.CharField(max_length=60, blank=True)
-    timestamp = models.DateTimeField(default=timezone.now())
+    timestamp = models.DateTimeField(timezone.now())
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile", primary_key=True)
     @receiver(post_save, sender=User)
     def create_user_profile(sender, instance, created, **kwargs):
@@ -28,3 +28,14 @@ class Profile(models.Model):
 
     def delete_profile(self):
             self.delete()  
+
+class Project(models.Model):
+    title = models.CharField(max_length=60, blank=True)
+    # image = CloudinaryField('image')
+    description = models.TextField(max_length=500)
+    link = models.URLField(blank=True)
+    user = models.ForeignKey(User, null=True,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,null=True,on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
