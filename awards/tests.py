456e21e3cfe58b76f1fc1fb0a5e007494da31c2d
kimutaiamos <amos.kimutai@student.moringashool.com>
2021-12-11 09:56:43 +0300

    added project testcase

--- awards/tests.py
+++ awards/tests.py
@@ -24,4 +24,32 @@ class ProfileTestClass(TestCase):
         self.profile.save_profile()
         profiles = Profile.objects.all()
         self.assertTrue(len(profiles) > 0)
+    
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile) == 0)
 
+
+        #project test class
+
+class ProjectTestClass(TestCase):
+    def setUp(self):
+        self.project = Project(title = 'baboon', image='baboon.jpg', description="baboon",link="https://en.wikipedia.org/wiki/Tiger")
+
+    def tearDown(self):
+        Project.objects.all().delete()
+
+    def test_instance(self):
+         self.assertTrue(isinstance(self.project, Project))
+    def test_save_method(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)>0)
+    def test_delete_method(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects)==0)
