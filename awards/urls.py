2b9c8da4557c2e3798e733060867516ac2784c00
kimutaiamos <amos.kimutai@student.moringashool.com>
2021-12-11 11:16:57 +0300

    added urls configurations

--- awards/urls.py
+++ awards/urls.py
@@ -8,4 +8,14 @@ urlpatterns=[
     url(r'^$',views.index,name='index'),
     url('registration/',views.registration, name='registration'),
     url('login/',auth_views.LoginView.as_view(), name='login'),
+    url('logout/',auth_views.LogoutView.as_view(), name='logout'),
+    url('profile/', views.profile, name='profile'),
+    url(r'^upload/$',views.new_project,name='add_project'),
+    url(r'^project_details/(?P<id>\d+)', views.project_details, name='projectdetails'),
+    url(r'^review/(?P<project_id>\d+)', views.review_project, name='review'),
+    url(r'^searched/', views.search_projects, name='search'),
+    url('api/profilelist',views.ProfileList.as_view(),name='profileEndpoint'),
+    url('api/projectslist',views.ProjectList.as_view(),name='projectsEndpoint')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
